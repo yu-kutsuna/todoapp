ef058ed3730c6ac0ab39320fcc9b4c06f3e59087
沓名 優平 <kutsuna_yuhei_gn@MICROAD-PC-551.local>
2020-02-03 18:58:25 +0900

    complete済みのアイテムのチェックボックスを非表示にする

--- app/src/main/java/yu/kutsuna/todoapp/TodoRowViewModel.kt
+++ app/src/main/java/yu/kutsuna/todoapp/TodoRowViewModel.kt
@@ -1,9 +1,12 @@
 package yu.kutsuna.todoapp
 
 import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TodoRowViewModel(val id: String, private val parentViewModel: MainViewModel): ViewModel() {
+    var isCompleted: MutableLiveData<Boolean> = MutableLiveData<Boolean>().apply { value = false }
+
     fun clickDeleteIcon(view: View) {
         parentViewModel.clickDeleteIcon(id)
     }
--- app/src/main/java/yu/kutsuna/todoapp/TodoViewAdapter.kt
+++ app/src/main/java/yu/kutsuna/todoapp/TodoViewAdapter.kt
@@ -11,12 +11,14 @@ import yu.kutsuna.todoapp.data.Todo
 import yu.kutsuna.todoapp.databinding.TodoRowItemBinding
 
 
-class TodoViewAdapter(private var todoList: List<Todo>,
-                      private val parentLifecycleOwner: LifecycleOwner,
-                      private val parentViewModel: MainViewModel
-): RecyclerView.Adapter<TodoViewAdapter.TodoViewHolder>() {
+class TodoViewAdapter(
+    private var todoList: List<Todo>,
+    private val parentLifecycleOwner: LifecycleOwner,
+    private val parentViewModel: MainViewModel
+) : RecyclerView.Adapter<TodoViewAdapter.TodoViewHolder>() {
+    private var todoRowViewModel: TodoRowViewModel? = null
 
-    class TodoViewHolder(val binding: TodoRowItemBinding): RecyclerView.ViewHolder(binding.root)
+    class TodoViewHolder(val binding: TodoRowItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     enum class AllSelectType {
         NONE,
@@ -27,7 +29,7 @@ class TodoViewAdapter(private var todoList: List<Todo>,
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): TodoViewHolder{
+    ): TodoViewHolder {
         val binding = DataBindingUtil.inflate<TodoRowItemBinding>(
             LayoutInflater.from(parent.context),
             R.layout.todo_row_item,
@@ -37,11 +39,12 @@ class TodoViewAdapter(private var todoList: List<Todo>,
         return TodoViewHolder(binding)
     }
 
-    override fun getItemCount(): Int  = todoList.size
+    override fun getItemCount(): Int = todoList.size
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        todoRowViewModel = TodoRowViewModel(todoList[position].id.toString(), parentViewModel)
         holder.binding.todo = todoList[position]
-        holder.binding.viewModel = TodoRowViewModel(todoList[position].id.toString(), parentViewModel)
+        holder.binding.viewModel = todoRowViewModel
         holder.binding.lifecycleOwner = parentLifecycleOwner
 
         /**
@@ -51,10 +54,10 @@ class TodoViewAdapter(private var todoList: List<Todo>,
          * アイテムが一つでもチェックされた時にクリアボタンを表示する
          */
         holder.binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
+            if (isChecked) {
                 parentViewModel.checkedIdList.add(todoList[position].id.toString())
             } else {
-                run loop@ {
+                run loop@{
                     parentViewModel.checkedIdList.forEachIndexed { index, id ->
                         if (id == todoList[position].id.toString()) {
                             parentViewModel.checkedIdList.removeAt(index)
@@ -72,20 +75,21 @@ class TodoViewAdapter(private var todoList: List<Todo>,
          * 一つでも選択されている場合は選択されていないチェックボックスを選択済みにする
          * 全て選択されている場合は全てのチェックボックスの選択を解除する
          */
-        when(allSelectType) {
+        when (allSelectType) {
             AllSelectType.ALL_SELECT -> holder.binding.checkBox.isChecked = true
             AllSelectType.ALL_CLEAR -> holder.binding.checkBox.isChecked = false
             AllSelectType.NONE -> Unit
         }
-        if(position + 1 == todoList.size) {
+        if (position + 1 == todoList.size) {
             allSelectType = AllSelectType.NONE
         }
 
         Log.d(TAG, "position $position , isCompleted ${todoList[position].isCompleted}")
-        if(todoList[position].isCompleted) {
+        if (todoList[position].isCompleted) {
             val paint = holder.binding.todoValue.paint
             paint.flags = holder.binding.todoValue.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             paint.isAntiAlias = true
+            todoRowViewModel?.isCompleted?.value = true
         } else {
             val paint = holder.binding.todoValue.paint
             paint.flags = 0
@@ -99,7 +103,7 @@ class TodoViewAdapter(private var todoList: List<Todo>,
     }
 
     fun allSelect() {
-        allSelectType = if(parentViewModel.checkedIdList.size < todoList.size) {
+        allSelectType = if (parentViewModel.checkedIdList.size < todoList.size) {
             AllSelectType.ALL_SELECT
         } else {
             AllSelectType.ALL_CLEAR
