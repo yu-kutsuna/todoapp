69ce9574aba409ba277cf2820f8733adad7a93b9
yuhei.kutsuna <g0832082@gmail.com>
2022-05-05 22:09:09 +0900

    fix: 広告削除

--- app/src/main/java/yu/kutsuna/todoapp/main/MainActivity.kt
+++ app/src/main/java/yu/kutsuna/todoapp/main/MainActivity.kt
@@ -1,35 +1,23 @@
 package yu.kutsuna.todoapp.main
 
-import android.app.ActionBar
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import android.view.View
-import android.widget.RemoteViews
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.initialization.InitializationStatus
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
 import yu.kutsuna.todoapp.R
 import yu.kutsuna.todoapp.TodoAppWidgetProvider
 import yu.kutsuna.todoapp.data.TodoModel
 import yu.kutsuna.todoapp.databinding.ActivityMainBinding
 import yu.kutsuna.todoapp.extensions.hideKeyboard
 import yu.kutsuna.todoapp.row.TodoViewAdapter
-import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity(), LifecycleOwner {
@@ -40,38 +28,6 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        MobileAds.initialize(this)
-        val adRequest = AdRequest.Builder().build()
-        binding.adView.loadAd(adRequest)
-        binding.adView.adListener = object : AdListener() {
-
-            override fun onAdClicked() {
-                super.onAdClicked()
-                Log.d(TAG, "AdMob onAdClicked")
-            }
-
-            override fun onAdClosed() {
-                super.onAdClosed()
-                Log.d(TAG, "AdMob onAdClosed")
-            }
-
-            override fun onAdImpression() {
-                super.onAdImpression()
-                Log.d(TAG, "AdMob onAdImpression")
-            }
-
-            override fun onAdLoaded() {
-                super.onAdLoaded()
-                Log.d(TAG, "AdMob onAdLoaded")
-            }
-
-            override fun onAdOpened() {
-                super.onAdOpened()
-                Log.d(TAG, "AdMob onAdOpened")
-            }
-        }
-
 
         // ViewModel取得
         mainViewModel =
